071420f7277efe4e37bc30ac348753cab09c4a89
Martin Ndibui <muhingan@outlook.com>
2018-09-25 17:14:16 +0300

    [Dialog, Upload] - remove old components

--- file-upload/src/app/upload/directives/file-dropzone.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileDropzoneDirective } from './file-dropzone.directive';
-
-describe('FileDropzoneDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileDropzoneDirective();
-    expect(directive).toBeTruthy();
-  });
-});

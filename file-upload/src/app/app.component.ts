9863db12252f513c8d2a75abb088dd90c90065fa
Martin Ndibui <muhingan@outlook.com>
2018-09-25 17:15:24 +0300

    [Icons] - added svg icons to app

--- file-upload/src/app/app.component.ts
+++ file-upload/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'cancel',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/cancel.svg'));
+    iconRegistry.addSvgIcon(
+      'file-pdf',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/file-pdf.svg'));
+  }
+  ngOnInit(): void {
+
+  }
 }
